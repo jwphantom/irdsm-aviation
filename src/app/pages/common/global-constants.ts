e1d5d1856669911da6509f3c3951290f08e04754
jwphantom <44881349+jwphantom@users.noreply.github.com>
2022-11-30 19:03:41 +0100

    change link server

--- src/app/pages/common/global-constants.ts
+++ src/app/pages/common/global-constants.ts
@@ -1,12 +1,30 @@
 export class GlobalConstants {
-    public static apiURL: string = "https://server-irdsm-aviation.herokuapp.com/api";
+    //old server on heroku
+    //public static apiURL: string = "https://server-irdsm-aviation.herokuapp.com/api";
+
+    //new server on https://render.com/
+    public static apiURL: string = "https://server-irdsm-aviation.onrender.comapi";
+
+    //local server
     //public static apiURL: string = "http://localhost:3001/api";
-    public static version: string = "1.2.0";
+
+    //app version
+    public static version: string = "1.3.0";
+
+    //year of current year
     public static year: string = "2022";
+
+    //email website
     public static email: string = "campus@irdsm-aviation";
+
+    //name of websitz
     public static appname: string = "IRDSM AVIATION"
+
+    //phone customer support
     public static phone1: string = "(+237) 698 885 161"
     public static phone2: string = "(+237) 673 908 319"
+
+    //localisation of campus
     public static localisation: string = "Cameroun, Yaoundé, Mballa2, Entrée Jamot, 1ère entrée à droite, Deuxième résidence";
 
 }
