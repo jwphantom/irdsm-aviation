1e7039a1243fb5dc1d26063cadba053fa77d3727
jwphantom <44881349+jwphantom@users.noreply.github.com>
2022-12-10 13:55:49 +0100

    fix bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { LocaleService } from './services/locale.service';
-import { Router, NavigationStart, NavigationEnd, NavigationCancel, NavigationError, Event, ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 import { filter } from 'rxjs/operators';
 import { Title } from '@angular/platform-browser';
 
-declare const gtag: Function; // <------------Important: the declartion for gtag is required!
 
 
 @Component({
@@ -18,15 +17,9 @@ export class AppComponent implements OnInit {
 
   constructor(
     private translate: TranslateService,
-    private activatedRoute: ActivatedRoute,
     private localeservice: LocaleService,
     private router: Router) {
 
-    this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        gtag('config', 'G-TE5VY08SY7', { 'page_path': event.urlAfterRedirects });
-      }
-    })
     translate.setDefaultLang(this.localeservice.lang);
   }
 
